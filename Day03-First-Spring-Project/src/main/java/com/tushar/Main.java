9c4faff494f71970910cf951c2c99eca8eca6284
itstushar8208 <itstushar8208@gmail.com>
2026-06-23 20:03:45 +0530

    Day 3 - First Spring Bean using XML Configuration

---
+++ Day03-First-Spring-Project/src/main/java/com/tushar/Main.java
@@ -0,0 +1,17 @@
+package com.tushar;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("beans.xml");
+
+        Student student =
+                (Student) context.getBean("student");
+
+        student.study();
+    }
+}
---
+++ Day03-First-Spring-Project/src/main/java/com/tushar/Student.java
@@ -0,0 +1,8 @@
+package com.tushar;
+
+public class Student {
+
+    public void study() {
+        System.out.println("Student is studying Spring Framework");
+    }
+}
