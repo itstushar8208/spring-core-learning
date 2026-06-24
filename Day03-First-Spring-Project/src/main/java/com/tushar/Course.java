9f7f1296384fc9749f1a1e483bfc9bb80ee3e9f0
itstushar8208 <itstushar8208@gmail.com>
2026-06-24 12:26:54 +0530

    Day 4 - Setter Dependency Injection

---
+++ Day03-First-Spring-Project/src/main/java/com/tushar/Course.java
@@ -0,0 +1,14 @@
+package com.tushar;
+
+public class Course {
+
+    private String courseName;
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
